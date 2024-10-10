b702458b4f97ec3c3bef4baacfbba5ecaaf452a9
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-10-10 15:06:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ docker build -t find_pairs_image ./games/find_pairs_game
 docker run -itd --name find_pairs_cont -p 1001:80 find_pairs_image
 docker build -t tic_tac_toe_image ./games/tic_tac_toe_game
 docker run -itd --name tic_tac_toe_cont -p 1002:80 tic_tac_toe_image
+docker build -t dashboard_image ./games
+docker run -itd --name dashboard_cont -p 1005:80 dashboard_image
