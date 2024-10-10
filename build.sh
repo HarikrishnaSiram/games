e7ad189bdc6185164f81a4f41766f043d78c645c
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-10-10 14:58:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ docker ps
 docker kill $(docker ps)
 docker rm $(docker ps -a)
 docker rmi $(docker images)
-docker build -t colour_sequence_image /root/games/games/colour_sequence_game
+docker build -t colour_sequence_image /games/colour_sequence_game
 docker run -itd --name colour_sequence_cont -p 1000:80 colour_sequence_image
-docker build -t find_pairs_image /root/games/games/find_pairs_game
+docker build -t find_pairs_image /games/find_pairs_game
 docker run -itd --name find_pairs_cont -p 1001:80 find_pairs_image
-docker build -t tic_tac_toe_image /root/games/games/tic_tac_toe_game
+docker build -t tic_tac_toe_image /games/tic_tac_toe_game
 docker run -itd --name tic_tac_toe_cont -p 1002:80 tic_tac_toe_image
